63fbfe64c6b5a8e73df2bf4826e0e98e5b55bab1
Timur  w6itec6apel <w6itec6apel@Timurs-MacBook-Air.local>
2024-05-19 16:39:52 +0300

    models.py edited

--- app/catalog/models.py
+++ app/catalog/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=100, default='Ivan')
     last_name = models.CharField(max_length=100, default='Ivanov')
-    middle_name = models.CharField(max_length=100, default='Ivanovich')  # Отчество
-    position = models.CharField(max_length=100, default='scientist')  # Должность
-    institute = models.CharField(max_length=100, default='IPMEIT')  # Институт
+    middle_name = models.CharField(max_length=100, default='Ivanovich')  
+    position = models.CharField(max_length=100, default='scientist')  
+    institute = models.CharField(max_length=100, default='IPMEIT') 
 
     def __str__(self):
         return f"{self.last_name} {self.first_name} {self.middle_name}"
