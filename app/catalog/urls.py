096086ac2b99b3edd251e8947d243e9d61a9ac3a
Timur  w6itec6apel <w6itec6apel@Timurs-MacBook-Air.local>
2024-05-20 12:56:03 +0300

    README.md edited

--- app/catalog/urls.py
+++ app/catalog/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('pdf/<int:pdf_id>/', views.pdf_view, name='pdf_view'),
     path('add/', views.add_pdf, name='add_pdf'),  # новый маршрут для добавления статьи
+    path('pdf/<int:pdf_id>/delete/', views.delete_pdf, name='delete_pdf'),
+    path('authors/', views.author_list, name='author_list'),
+    path('authors/<int:author_id>/', views.author_detail, name='author_detail'),
 ]
--- app/catalog/views.py
+++ app/catalog/views.py
@@ -4,15 +4,25 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 
 def pdf_view(request, pdf_id):
     pdf = get_object_or_404(PDFFile, pk=pdf_id)
-    return render(request, 'pdf_view.html', {'pdf': pdf})
+    is_author = request.user.is_authenticated and hasattr(request.user, 'author')
+    return render(request, 'pdf_view.html', {'pdf': pdf, 'is_author': is_author})
+
+def is_author(user):
+    return hasattr(user, 'author')
 
 def index(request):
     pdfs = PDFFile.objects.all()
     is_author = request.user.is_authenticated and hasattr(request.user, 'author')
     return render(request, 'index.html', {'pdfs': pdfs, 'is_author': is_author})
 
-def is_author(user):
-    return hasattr(user, 'author')
+@login_required
+@user_passes_test(is_author)
+def delete_pdf(request, pdf_id):
+    pdf = get_object_or_404(PDFFile, pk=pdf_id)
+    if request.method == 'POST':
+        pdf.delete()
+        return redirect('index')
+    return render(request, 'confirm_delete.html', {'pdf': pdf})
 
 @login_required
 @user_passes_test(is_author)
@@ -29,8 +39,17 @@ def add_pdf(request):
         for author_id in author_ids:
             author = Author.objects.get(pk=author_id)
             pdf.authors.add(author)
+            author.pdf_file_set.add(pdf)
 
         return redirect('index')
     else:
         authors = Author.objects.all()
         return render(request, 'add_pdf.html', {'authors': authors})
+    
+def author_list(request):
+    authors = Author.objects.all()
+    return render(request, 'author_list.html', {'authors': authors})
+
+def author_detail(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    return render(request, 'author_detail.html', {'author': author})
