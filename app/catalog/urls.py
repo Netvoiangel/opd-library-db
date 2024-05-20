211cd00a7058e111259dc84b298d193cc1a1830c
Timur  w6itec6apel <w6itec6apel@Timurs-MacBook-Air.local>
2024-05-20 11:31:24 +0300

    Хуйня полная но скоро заработает

--- app/catalog/urls.py
+++ app/catalog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('pdf/<int:pdf_id>/', views.pdf_view, name='pdf_view'),
+    path('add/', views.add_pdf, name='add_pdf'),  # новый маршрут для добавления статьи
 ]
--- app/catalog/views.py
+++ app/catalog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import PDFFile
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import PDFFile, Author
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 def pdf_view(request, pdf_id):
     pdf = get_object_or_404(PDFFile, pk=pdf_id)
@@ -7,4 +8,29 @@ def pdf_view(request, pdf_id):
 
 def index(request):
     pdfs = PDFFile.objects.all()
-    return render(request, 'index.html', {'pdfs': pdfs})
+    is_author = request.user.is_authenticated and hasattr(request.user, 'author')
+    return render(request, 'index.html', {'pdfs': pdfs, 'is_author': is_author})
+
+def is_author(user):
+    return hasattr(user, 'author')
+
+@login_required
+@user_passes_test(is_author)
+def add_pdf(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        direction = request.POST.get('direction')
+        pdf_file = request.FILES.get('pdf_file')
+        author_ids = request.POST.getlist('authors')
+
+        pdf = PDFFile(title=title, direction=direction, pdf=pdf_file)
+        pdf.save()
+
+        for author_id in author_ids:
+            author = Author.objects.get(pk=author_id)
+            pdf.authors.add(author)
+
+        return redirect('index')
+    else:
+        authors = Author.objects.all()
+        return render(request, 'add_pdf.html', {'authors': authors})
